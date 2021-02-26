3e70e22887542e995f3ce711e21035f32c6a17cf
Daniel Argueso <d.argueso@uib.es>
2021-02-26 16:27:01 +0100

    Automatic AEMET data update

--- git_commit.sh
+++ git_commit.sh
@@ -1,3 +1,6 @@
+this_dir = $pwd
+cd /home/dargueso/Scripts/webhourlydata/
 git add -A;
-git commit -m "$1";
-git push origin $2
+git commit -m "Automatic AEMET data update";
+git push origin main 
+cd $this_dir
